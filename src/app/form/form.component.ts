ac3448d6352e6f5366cf83b2aa099b60e1ed9c0d
BarbaraM <babicilagrr@gmail.com>
2023-07-30 13:23:11 +0100

    adding simple validators and submit button disabled

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -15,21 +15,24 @@ export class FormComponent implements OnInit{
 
   ngOnInit(){
     this.form = this.formBuilder.group({
-      name: [null],
-      email: [null],
-      nif: [null],
-      nascimento: [null],
-      pais: [null],
-      cidade: [null],
-      endereco: [null],
-      codigopostal: [null],
-      telefone: [null],
-      genero: [null],
+      name: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      nif: [null, [Validators.required]],
+      nascimento: [null,[Validators.required]],
+      pais: [null, [Validators.required]],
+      cidade: [null, [Validators.required]],
+      endereco: [null, Validators.required],
+      codigopostal: [null, [Validators.required]],
+      telefone: [null, [Validators.required]],
+      genero: [null, [Validators.required]],
     })
   }
   showForm(){
     return JSON.stringify(this.form.value)
   }
 
-
+  onSubmit(){
+    console.log(this.form);
+    //this.form.reset();
+  }
 }
